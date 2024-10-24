c460570136fd8f1f7fa2307eef93cb486083baed
aboy <1747193328@qq.com>
2024-10-24 14:52:16 +0800

    1

---
+++ src/NepTrain/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/10/24 14:42
+# @Author  : 兵
+# @email    : 1747193328@qq.com
+__version__ = '0.1.0'
---
+++ src/NepTrain/cli/NepTrain.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+# @Time    : 2024/10/24 14:33
+# @Author  : 兵
+# @email    : 1747193328@qq.com
+
+from NepTrain.core import a
+if __name__ == '__main__':
+    pass
