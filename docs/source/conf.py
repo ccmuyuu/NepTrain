e81012ccc23386e4083d37395ceb3aff2c9d50a6
aboy <1747193328@qq.com>
2024-12-09 19:54:28 +0800

    1.5.0 加入OpenMP

---
+++ docs/source/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+
+project = 'NepTrainKit'
+copyright = '2024, ChengBing Chen,YuTong Li'
+author = 'ChengBing Chen'
+release = '1.4.9'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # 'sphinx.ext.autodoc',
+    # 'sphinx.ext.napoleon',
+    # 'sphinx.ext.mathjax',
+
+"sphinx_design",
+    "myst_parser"
+]
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    # "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+templates_path = ['_templates']
+# locale_dirs = ['docs/locales']  # 语言文件目录
+language = 'en'
+
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
