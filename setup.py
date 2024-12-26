8646f2f524221e0edc9fadcfb17a5917cbd5c1ee
aboy <1747193328@qq.com>
2024-12-26 16:09:28 +0800

    1.5.5

--- setup.py
+++ setup.py
@@ -25,6 +25,8 @@ pybind11_include = pybind11.get_include()
 
 extra_link_args = []
 
+ 
+
 # 检查平台并设置相应的 OpenMP 编译标志
 
 if sys.platform == "win32":
@@ -35,11 +37,13 @@ if sys.platform == "win32":
 elif sys.platform == "darwin":
     # 对于 macOS 和 Clang 使用 -fopenmp 编译标志
     extra_compile_args = ['-fopenmp' ]
+    extra_link_args.append('-fopenmp')
 
 
 else:
     # 对于 Linux 和 GCC 使用 -fopenmp 编译标志
     extra_compile_args = ['-fopenmp']
+    extra_link_args.append('-fopenmp')
 # 定义扩展模块
 ext_modules = [
     Pybind11Extension(
